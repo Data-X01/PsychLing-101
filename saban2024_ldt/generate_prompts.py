ac990564dbb53e150947d08c6f90b6cf59c676f9
iva-saban <iva.saban@unimib.it>
2026-05-07 16:32:49 +0200

    Latest update

--- saban2024_ldt/generate_prompts.py
+++ saban2024_ldt/generate_prompts.py
@@ -5,11 +5,11 @@ import string
 from pathlib import Path
 
 #Define the path
-path = Path("C:/Users/ivasa/Documents/PsyLing101/My contribution - Italian LDT with colour words/Preparation of data/Experiment 1 - colour associates") / "prompts.jsonl"
+path = Path("C:/Users/ivasa/Documents/PsyLing101/My contribution - Italian LDT with colour words/Fixing again") / "prompts.jsonl"
 
 #Load preprocessed files from Exp1 and Exp2
-df1 = pd.read_csv("C:/Users/ivasa/Documents/PsyLing101/My contribution - Italian LDT with colour words/Fixing the errors/processed_data/exp1.csv", sep=';')
-df2 = pd.read_csv("C:/Users/ivasa/Documents/PsyLing101/My contribution - Italian LDT with colour words/Fixing the errors/processed_data/exp2.csv", sep=';')
+df1 = pd.read_csv("C:/Users/ivasa/Documents/PsyLing101/My contribution - Italian LDT with colour words/Fixing again/processed_data/exp1.csv", sep=';')
+df2 = pd.read_csv("C:/Users/ivasa/Documents/PsyLing101/My contribution - Italian LDT with colour words/Fixing again/processed_data/exp2.csv", sep=';')
 
 #Merge the two dataframes
 df = pd.concat([df1, df2], ignore_index=True)
@@ -19,7 +19,10 @@ participants = df['participant_id'].unique() #This puts each participant in a se
 trials = range(df['trial_id'].max() + 1)
 
 #Recode response keys
-df["key"] = df["key"].map({1: "a", 2: "l"})
+df["response"] = df["response"].map({1: "a", 2: "l"})
+
+#Recode accuracy
+df["accuracy"] = df["accuracy"].map({1: "1", 0: "0", 3: "2"})
 
 # Generate individual prompts for each participant
 all_prompts = []
@@ -37,17 +40,19 @@ for participant in participants:
     age = df_participant['age'].iloc[0].item()
 
     rt_list = []
+    accuracy_list = []
 
     prompt = f"""In questo studio vi verranno mostrati diversi stimoli sullo schermo. Il vostro compito è quello di determinare se si tratta di una parola italiana esistente o meno.\n 
 Se pensate che sia una parola italiana esistente, premete il tasto {choices[0]}, mentre se pensate che non sia una parola italiana esistente, premete il tasto {choices[1]}.\n
 Ignorate il colore dello stimolo e concentratevi sull'esistenza o meno della parola in italiano.\n"""
 
     for _, row in df_participant.iterrows():
-        word = row['stimuli']
-        response = row['key']
+        word = row['stimulus']
+        response = row['response']
 
         prompt += f'The word "{word}" appears on the screen. You press <<{response}>>.\n'
         rt_list.append(row['rt'])
+        accuracy_list.append(row['accuracy'])
 
     all_prompts.append({
         'text': prompt,
@@ -55,6 +60,7 @@ Ignorate il colore dello stimolo e concentratevi sull'esistenza o meno della par
         'participant_id': participant,
         'age': age,
         'rt': rt_list,
+        'accuracy': accuracy_list
     })
 
 #Save all prompts to JSONL file
