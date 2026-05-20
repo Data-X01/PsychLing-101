8e1cbacb4633dde25a0e111cfac340a9bd10969d
Cheng-Yu Hsieh <cyhsieh@Cheng-Yu-Hsiehs-MacBook-Air.local>
2026-05-20 16:57:57 +0800

    Add hsieh2025_rating experiment files

---
+++ hsieh2025_rating/preprocess_data.py
@@ -0,0 +1,20 @@
+# Preprocessing data of hsieh2025_rating
+
+# Load Libraries
+import pandas as pd
+
+# Read the raw data
+df = pd.read_csv("/Users/cyhsieh/PsychLing-101/hsieh2025_rating/original_data:/meaningfulness_rating.csv")
+
+# Rename varaibles based on the codebook
+rename_map = {
+    "participant": "participant_id",
+    "item": "stimulus",
+    "num": "trial_id",
+    "rating": "response"
+}
+
+df_cleaned = df.rename(columns=rename_map)
+
+# Export the cleaned file
+df_cleaned.to_csv("/Users/cyhsieh/PsychLing-101/hsieh2025_rating/processed_data:/rating.csv", index=False)
